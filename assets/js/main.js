9954f6d02bf569ebf9d30f86e8fd9b0ca9ead2f2
karfaisarra <karfaisarra@gmail.com>
2022-10-13 14:25:50 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+const shoppingList = [
+    'Compra pane',
+    'Latte di marndorle',
+    'Insalata',
+    'Burger di soia'
+  ];
+  
+  
+  
+  const shoppingListElement = document.querySelector('.shopping-list')
+  console.log(shoppingListElement);
+  
+  
+  /* for (let i = 0; i < shoppingList.length; i++) {
+    const item = shoppingList[i];
+  
+    const liElement = `<li>${item}</li>`
+    shoppingListElement.insertAdjacentHTML('beforeend', liElement)
+  
+  } */
+
+  //while loop
+  let i = 0;
+  while (i < shoppingList.length) {
+    const item = shoppingList[i];
+  
+    const liElement = `<li>${item}</li>`
+    shoppingListElement.insertAdjacentHTML('beforeend', liElement)
+
+    i++
+  }
